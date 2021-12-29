4199822a74e4d95b97b31d6c23a782cc663013bb
Alejandro Balderas <alejandro@balderas.de>
2021-12-29 17:43:10 +0100

    Add url the only list in the world

--- lists/tests.py
+++ lists/tests.py
@@ -1,16 +1,11 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
 from lists.views import home_page
 from lists.models import Item
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
@@ -26,40 +21,39 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/")
+        self.assertEqual(response["location"], "/lists/the-only-list-in-the-world/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
 
+
+class ListViewtest(TestCase):
     def test_displays_all_list_items(self):
         Item.objects.create(text="itemey 1")
         Item.objects.create(text="itemey 2")
 
-        response = self.client.get("/")
+        response = self.client.get("/lists/the-only-list-in-the-world/")
 
-        self.assertIn("itemey 1", response.content.decode())
-        self.assertIn("itemey 2", response.content.decode())
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_text = "The first (ever) list item"
         second_text = "Item the second"
-
         first_item = Item()
         first_item.text = first_text
-        first_item.save()
-
         second_item = Item()
         second_item.text = second_text
+
+        first_item.save()
         second_item.save()
-
         saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
 
+        self.assertEqual(saved_items.count(), 2)
         self.assertEqual(first_saved_item.text, first_text)
         self.assertEqual(second_saved_item.text, second_text)
