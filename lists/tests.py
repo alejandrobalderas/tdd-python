dfe16cc5d8a28649bf43c06c97d8fee6bbbc6303
Alejandro Balderas <alejandro@balderas.de>
2022-01-03 11:57:42 +0100

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -29,7 +29,11 @@ class HomePageTest(TestCase):
 
 
 class ListViewtest(TestCase):
-    def test_displays_all_list_items(self):
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_displays_all_items(self):
         Item.objects.create(text="itemey 1")
         Item.objects.create(text="itemey 2")
 
