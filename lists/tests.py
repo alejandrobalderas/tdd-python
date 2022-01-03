228f186a14dd60393711e82d4634146ce82e88f3
Alejandro Balderas <alejandro@balderas.de>
2022-01-03 12:31:26 +0100

    Items are associated with different lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -29,8 +29,9 @@ class ListViewtest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="itemey 1", list=list_)
+        Item.objects.create(text="itemey 2", list=list_)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
@@ -38,21 +39,32 @@ class ListViewtest(TestCase):
         self.assertContains(response, "itemey 2")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_text = "The first (ever) list item"
         second_text = "Item the second"
         first_item = Item()
         first_item.text = first_text
+        first_item.list = list_
         second_item = Item()
         second_item.text = second_text
+        second_item.list = list_
 
         first_item.save()
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
         saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-
-        self.assertEqual(saved_items.count(), 2)
         self.assertEqual(first_saved_item.text, first_text)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, second_text)
+        self.assertEqual(second_saved_item.list, list_)
