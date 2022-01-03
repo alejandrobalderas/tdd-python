d58a877fd4d9ab9a6d9cced73650353e74ef410c
Alejandro Balderas <alejandro@balderas.de>
2022-01-03 13:56:36 +0100

    new URL + view for adding to existing lists. FT passes :)

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.urls import resolve
 from django.test import TestCase
 
@@ -45,6 +46,12 @@ class ListViewtest(TestCase):
         self.assertNotContains(response, "other itemey 1")
         self.assertNotContains(response, "other itemey 2")
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f"/lists/{correct_list.id}/")
+        self.assertEqual(response.context["list"], correct_list)
+
 
 class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -75,3 +82,29 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, second_text)
         self.assertEqual(second_saved_item.list, list_)
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for an existing list"},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new item for an existing list")
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for an existing list"},
+        )
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
