04361f098b0dd100d847f3555ff32be3262b46e7
Alejandro Balderas <alejandro@balderas.de>
2022-01-04 14:51:53 +0100

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -53,37 +53,6 @@ class ListViewtest(TestCase):
         self.assertEqual(response.context["list"], correct_list)
 
 
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_text = "The first (ever) list item"
-        second_text = "Item the second"
-        first_item = Item()
-        first_item.text = first_text
-        first_item.list = list_
-        second_item = Item()
-        second_item.text = second_text
-        second_item.list = list_
-
-        first_item.save()
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, first_text)
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, second_text)
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
